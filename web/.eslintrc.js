46116aa85f39c432b16e0e737a702238bb8916bd
imamura.tomoki <nowvilla.physi.311@gmail.com>
2025-04-30 19:56:14 +0900

    lintとformatterを追加した

---
+++ web/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended"
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react", "prettier"],
+    rules: {
+        "prettier/prettier": "warn",
+        "react/react-in-jsx-scope": "off"
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+};
